b5a05ceed46587255be3d909eab5db08e0620ab5
孙亚锋 <sun@leimo22.local>
2017-06-29 16:18:20 +0800

    Initial Commit

---
+++ YYKit_labelTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYKit_labelTest
+//
+//  Created by 孙亚锋 on 2017/6/29.
+//  Copyright © 2017年 LeiMo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYKit_labelTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYKit_labelTest
+//
+//  Created by 孙亚锋 on 2017/6/29.
+//  Copyright © 2017年 LeiMo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
